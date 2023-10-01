8b5068153bc2013adba8492c2d71475668600c3d
5jisoo <56earls@gmail.com>
2023-10-01 23:08:00 +0900

    Feat: add filter using java code

---
+++ 오지수/api-gateway-service/src/main/java/com/example/apigatewayservice/common/config/FilterConfig.java
@@ -0,0 +1,27 @@
+package com.example.apigatewayservice.common.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        r -> r.path("/first-service/**") // 1. 경로 확인
+                                .filters(filter -> filter // 2. filter 적용
+                                        .addRequestHeader("first-request", "first-request-header")
+                                        .addResponseHeader("first-response", "first-response-header"))
+                                .uri("http://localhost:8081")) // 3. url 이동
+                .route(
+                        r -> r.path("/second-service/**")
+                                .filters(filter -> filter
+                                        .addRequestHeader("second-request", "second-request-header")
+                                        .addResponseHeader("second-response", "second-response-header"))
+                                .uri("http://localhost:8082"))
+                .build();
+    }
+}
