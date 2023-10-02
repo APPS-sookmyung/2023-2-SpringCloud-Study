7a63a9e212b2dfa2bb9f817f29b772f386b4ebaf
ajeong <ajung7038@naver.com>
2023-10-02 17:37:30 +0900

    Feat: Spring Cloud Gateway 프로젝트 생성

--- 주아정/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ 주아정/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,4 +1,14 @@
 package com.example.firstservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service/") // URI 지정 (루트)
 public class FirstServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service.";
+    }
 }
--- 주아정/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ 주아정/second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,14 +1,14 @@
-package com.example.firstservice;
+package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/") // URI 지정 (루트)
-public class FirstServiceController {
+@RequestMapping("/second-service/") // URI 지정 (루트)
+public class SecondServiceController {
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to the First service.";
+        return "Welcome to the Second service.";
     }
 }
