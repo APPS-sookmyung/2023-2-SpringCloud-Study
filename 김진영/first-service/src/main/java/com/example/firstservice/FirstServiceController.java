f2781737e51338b4c62775cce9b5c96a193a8d63
coldnoodle_kjy <kjy11069@sookmyung.ac.kr>
2023-10-04 03:15:24 +0900

    Feat: 과제 코드 추가

--- 김진영/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ 김진영/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 // http://localhost:8081/welcome
 // http://localhost:8081/first-service/welcome
 @RestController
@@ -22,4 +24,11 @@ public class FirstServiceController {
         log.info(header);
         return "Hello World in First Service.";
     }
+
+    @GetMapping("/3rd-homework")
+    public String homework(HttpServletRequest request) {
+        String header= request.getHeader("first-request");
+        return header;
+    }
+
 }
