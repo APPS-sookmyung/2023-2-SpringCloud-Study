6d5f346640cff1b09dcbed535c94cbf68bda1492
5jisoo <56earls@gmail.com>
2023-11-05 21:06:40 +0900

    Test: @Value, Environment

---
+++ 오지수/user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.userservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+}
