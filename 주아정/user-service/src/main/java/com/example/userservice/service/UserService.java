7bd4c12985dfe101b06554bf0c0b822d246f7122
ajeong <ajung7038@naver.com>
2023-11-15 15:52:36 +0900

    Feat: Users Microservice 구현

--- 주아정/user-service/src/main/java/com/example/userservice/service/UserService.java
+++ 주아정/user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -1,7 +1,11 @@
 package com.example.userservice.service;
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
 
 public interface UserService {
     UserDto createUser(UserDto userDto);
+    
+    UserDto getUserByUserId(String userId);
+    Iterable<UserEntity> getUserByAll(); // db에서 바로 가져옴 // 가공할 거면 UserDto로 바꿔도 가능
 }
