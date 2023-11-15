9bb621f46f61db1b06c45d9000de20c79e75c37f
5jisoo <56earls@gmail.com>
2023-11-15 19:51:10 +0900

    Fix: merge error

---
+++ 오지수/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,79 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.domain.UserEntity;
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.modelmapper.convention.MatchingStrategies.STRICT;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/user-service")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final Environment env;
+    private final Greeting greeting;
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser requestUser) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(STRICT);
+
+        UserDto userDto = mapper.map(requestUser, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(CREATED).body(responseUser);
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's Working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return env.getProperty("greeting.message");
+    }
+
+    @GetMapping("/welcome2")
+    public String welcome2() {
+        return greeting.getMessage();
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<ResponseUser>> getUsers() {
+        Iterable<UserEntity> userList = userService.getUserByAll();
+
+        List<ResponseUser> result = new ArrayList<>();
+        userList.forEach(v -> {
+            result.add(new ModelMapper().map(v, ResponseUser.class));
+        });
+
+        return ResponseEntity.status(OK).body(result);
+    }
+
+    @GetMapping("/users/{userId}")
+    public ResponseEntity<ResponseUser> getUser(@PathVariable("userId") String userId) {
+        UserDto userById = userService.getUserById(userId);
+
+        ResponseUser responseUser = new ModelMapper().map(userById, ResponseUser.class);
+
+        return ResponseEntity.status(OK).body(responseUser);
+    }
+}
