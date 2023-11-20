47c0f8085dd8ae14ff3d3955f3fff1a64ba2440d
ajeong <ajung7038@naver.com>
2023-11-21 01:08:49 +0900

    Feat: loadUserByUsername() 구현

--- 주아정/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ 주아정/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,19 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username); //username == email
+
+        if (userEntity == null) { // 해당 유저 존재 X
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true,
+                new ArrayList<>()); // 로그인이 되었을 때 권한 추가 -> 비어있는 ArrayList 형태로 반환
+    }
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
